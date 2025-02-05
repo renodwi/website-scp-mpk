4ceb79d07ea669e98f814092ca6491ca51b0198c
renodwi <abggilina@gmail.com>
2025-02-05 23:02:54 +0700

    update

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
         <div class="flex justify-between">
             <div class="flex flex-col justify-center">
                 <h1 class="font-sans text-white font-black uppercase text-lg md:text-2xl">
-                    SCHOOL CARE PROGRAME SMANSADE
+                    SCHOOL CARE PROGRAMME SMANSADE
                 </h1>
             </div>
             <div class="flex gap-0 md:gap-2">
